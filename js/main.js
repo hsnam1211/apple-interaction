e2601e44e87f88b3dabad6aff3682300ffa40267
hoosss <hsnam1211@naver.com>
2023-10-25 13:29:56 +0900

    .

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 (() => {
-
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
   let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
   let currentScene = 0; // 현재 활성화된 (눈 앞에 보고 있는) 씬(scroll-section)
@@ -11,8 +10,8 @@
       heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-0')
-      }
+        container: document.querySelector('#scroll-section-0'),
+      },
     },
     {
       // 1
@@ -20,8 +19,8 @@
       heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-1')
-      }
+        container: document.querySelector('#scroll-section-1'),
+      },
     },
     {
       // 2
@@ -29,8 +28,8 @@
       heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-2')
-      }
+        container: document.querySelector('#scroll-section-2'),
+      },
     },
     {
       // 3
@@ -38,8 +37,8 @@
       heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-3')
-      }
+        container: document.querySelector('#scroll-section-3'),
+      },
     },
   ];
 
@@ -47,8 +46,21 @@
     //각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
   function scrollLoop() {
@@ -59,23 +71,24 @@
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       currentScene++;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
     if (yOffset < prevScrollHeight) {
       // 스크롤 바운스 방지
-      if (currentScene === 0) return
+      if (currentScene === 0) return;
       currentScene--;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
-    console.log(currentScene)
   }
 
-  window.addEventListener('resize', setLayout);
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset;
     scrollLoop();
   });
-
+  // window.addEventListener('DOMContentLoaded', setLayout);
+  window.addEventListener('load', setLayout);
+  window.addEventListener('resize', setLayout);
 
   setLayout();
-  console.log(sceneInfo)
 })();
